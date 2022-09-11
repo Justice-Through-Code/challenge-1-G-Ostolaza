781616416478de1f0ddb167838119a3205fb402f
Gabriel Ostolaza <Gabe.Osto@gmail.com>
2022-09-10 22:15:03 -0700

    added several functions to calculate temperature conversion from F to C and vice versa

--- temp_conversion.py
+++ temp_conversion.py
@@ -1,30 +1,29 @@
 
 def convert_100_to_celsius():
-    # Convert a temperature of 100 degrees fahrenheit to celsius
-    # Save this to a variable called celsius_100, and use print() to print out the value
-    # Is the resulting temperature you get an integer or float?
-    # Print 'float' if it is a float or 'int' if it is an int
-    # How do you know? Write a comment below your code explaining how you know which it is
-
+    #converting fahrenheit to celsius
+    celsius_100 = (100 - 32)*5/9
+    print(celsius_100)
+    print('float')
 
 def convert_0_to_celsius():
-    # Convert a temperature of 0 degrees fahrenheit to celsius
-    # Save this to a variable called celsius_0, and use print() to print out the value
-
+    #converting fahrenheit to celsius
+    celsius_0 = (0 - 32)*5/9
+    print(celsius_0)
 
 def convert_34_2_to_celsius():
-    # Convert a temperature of 34.2 degrees fahrenheit to celsius
-    # Do this one all in one print statement without saving any variables
-
-'''
-Now, can you convert back?
-'''
+    #converting fahrenheit to celsius in print statement
+    print((34.2 - 32) * 5/9)
 
 def convert_5_to_fahrenheit():
-    # Convert a temperature of 5 degrees celsius to fahrenheit and print it out
-
+    #conerting from celsius to fahrenheit
+    fahrenheit_conversion = (5 * 9/5) + 32
+    print(fahrenheit_conversion)
 
 def hotter_temp():
-    # What is hotter, a temperature of 30.2 degrees celsius, or a temperature of 85.1 degrees fahrenheit?
-    # Print out the hotter temp: '30.2 degrees celsius' or '85.1 degrees fahrenheit', respectively
-
+    #converting celsius to fahrenheit
+    fahrenheit_conversion = (30.2 * 9/5) + 32
+    fahrenheit = 85.1
+    #checking the two fahrenheits against each other to find greater value
+    if fahrenheit_conversion > fahrenheit:
+        print('30.2 degrees celsius')
+    else: print('85.1 degrees fahrenheit')
